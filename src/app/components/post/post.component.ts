4ade187ffb9849a5adfe268274eeb67405994773
Harminder Singh <minder91singh@gmail.com>
2018-06-07 08:55:43 -0700

    Added Post Details and Not-Found Component

---
+++ src/app/components/post/post.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location} from '@angular/common';
+import {PostService} from '../../services/post.service';
+
+import {Post} from '../../models/Post';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+post:Post
+  constructor(
+    private route:ActivatedRoute,
+    private PostService:PostService,
+    private location:Location
+  ) { 
+  }
+
+  ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id')
+    this.PostService.getPost(id).subscribe(post=>{
+      this.post = post;
+    })
+  }
+
+}
